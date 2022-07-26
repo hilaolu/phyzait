1f07208b7f2ba0a07c1ce079818823b6879fb02d
hilaolu <i@phyzait.moe>
2022-07-26 10:36:48 +0800

    update

--- conf/xplr/init.lua
+++ conf/xplr/init.lua
@@ -1,4 +1,4 @@
-version = "0.18.0"
+version = "0.19.0"
 xplr.config.modes.builtin.default.key_bindings.on_key["j"] = xplr.config.modes.builtin.default.key_bindings.on_key.left
 xplr.config.modes.builtin.default.key_bindings.on_key[";"] = xplr.config.modes.builtin.default.key_bindings.on_key.right
 xplr.config.modes.builtin.default.key_bindings.on_key["k"] = xplr.config.modes.builtin.default.key_bindings.on_key.up
