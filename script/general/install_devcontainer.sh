94e572b1f6f54f8b028f6f300899828dbd42f413
Phyzait Lu <i@phyzait.moe>
2023-04-09 09:06:56 +0800

    Create install_devcontainer.sh

---
+++ script/general/install_devcontainer.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt install fish -y
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+# fish script/general/apply-config.sh
