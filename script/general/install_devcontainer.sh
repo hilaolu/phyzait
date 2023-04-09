3e57c85baabf37f1cfc5e111b605d54508b20de1
Phyzait Lu <i@phyzait.moe>
2023-04-09 03:17:25 +0000

    update

--- script/general/install_devcontainer.sh
+++ script/general/install_devcontainer.sh
@@ -1,3 +1,5 @@
-curl https://github.com/microsoft/vscode-dev-containers/raw/main/script-library/fish-debian.sh -L | sudo bash
-fish $(dirname "$0")/apply-config.fish
-# fish $(dirname "$0")/fisher.fish
+export DEBIAN_FRONTEND=noninteractive
+mkdir -p ~/.config
+cp $(dirname "$0")/../../conf/fish ~/.config -r
+curl -L https://github.com/microsoft/vscode-dev-containers/raw/main/script-library/fish-debian.sh | sudo bash
+echo "fish -c 'fisher install IlanCosman/tide@v5'" | bash
