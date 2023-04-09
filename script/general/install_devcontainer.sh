aac21445d9acad2ac8507432516d18f81437141b
Phyzait Lu <i@phyzait.moe>
2023-04-09 02:12:31 +0000

    update

--- script/general/install_devcontainer.sh
+++ script/general/install_devcontainer.sh
@@ -1,4 +1,4 @@
 sudo apt update && sudo apt install fish -y
-sleep 1
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+# fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
 # fish script/general/apply-config.sh
+fish $(pwd)/apply-config.sh
