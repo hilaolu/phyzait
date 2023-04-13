cc074e4607da4e9ed41f5d6065b130a726e174eb
Phyzait Lu <i@phyzait.moe>
2023-04-13 08:14:51 +0000

    copy vim setting

--- script/general/install_devcontainer.sh
+++ script/general/install_devcontainer.sh
@@ -1,5 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 mkdir -p ~/.config
 cp $(dirname "$0")/../../conf/fish ~/.config -r
+cp $(dirname "$0")/../../conf/vim/.vimrc ~ -r
 curl -L https://github.com/microsoft/vscode-dev-containers/raw/main/script-library/fish-debian.sh | sudo bash
 echo "fish -c 'fisher install IlanCosman/tide@v5'" | bash
